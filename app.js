a8f77517bc25e66b41a33ab3e478b47735374f32
Ilya Uhanev <162031908+AllChatMute@users.noreply.github.com>
2025-04-29 13:34:02 +0300

    Added cors politics

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectToDataBase = require("./db");
+
 const authRouter = require("./routes/auth.route");
 const todosRouter = require("./routes/todos.route");
 
@@ -11,6 +13,13 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
